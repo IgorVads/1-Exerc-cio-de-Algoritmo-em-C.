0cf5d8247867d3442f136ca20a17003aeb579b6d
Igor Vinicius <161034058+IgorVads@users.noreply.github.com>
2024-04-14 07:36:39 -0300

    Update Fahrenheit_para_Celcius.c
    Feito a indentação do programa e adicionado o comentário explicando o objetivo do programa.

--- EX1/Fahrenheit_para_Celcius.c
+++ EX1/Fahrenheit_para_Celcius.c
@@ -2,12 +2,21 @@
 #include <math.h>
 #include <locale.h>
 
+/* Crie um programa que solicita e lê uma temperatura em Celsius, faz a conversão 
+para Fahrenheit e exibe tal resultado. Para isto, utilize a seguinte fórmula: 
+
+𝐹 = (𝐶 × 1,8) + 32  */
+
 int main() 
 { setlocale(LC_ALL, "portuguese");
+ 
 	float tempcl,tempfa;
+ 
 	printf("Digite a Temperatura (em Fahrenheit):");
-	scanf("%f", &tempfa);
+		scanf("%f", &tempfa);
+ 
 	tempcl = (tempfa - 32)/1.8;
-	printf("A sua temperatura em Celcius �: %.0f\n", tempcl);
+ 
+	printf("A sua temperatura em Celcius é: %.0f\n", tempcl);
 	return 0;
 }
