e4b6528a77db44ca11493ce6923df1d313b51132
Igor Vinicius <161034058+IgorVads@users.noreply.github.com>
2024-04-14 07:40:28 -0300

    Update Quadrado_de_um_número.c
    Indentação do código + comentário explicativo de objetivo do programa.

--- EX1/Quadrado_de_um_número.c
+++ EX1/Quadrado_de_um_número.c
@@ -2,12 +2,18 @@
 #include <math.h>
 #include <locale.h>
 
+/*Um algoritmo para calcular o quadrado de um nÃºmero.*/
+
 int main(int argc, char** argv)
 {	setlocale(LC_ALL, "portuguese");
+ 
 	float n1, result;
-	printf("Digite um número: ");
-	scanf("%f", &n1);
+ 
+	printf("Digite um nÃºmero: ");
+		scanf("%f", &n1);
+ 
 	result = pow(n1,2);
-	printf("O quadrado do seu número é: %.0f", result);
+ 
+	printf("O quadrado do seu nÃºmero Ã©: %.0f", result);
 	return 0;
 }
